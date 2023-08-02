b7df6886ce87a61d10661e3b44e70a7bb5f0249d
İlkin <ilkinhd@code.edu.az>
2023-08-02 19:29:00 +0400

    Added Profiles

---
+++ Book.Service/Profiles/Categories/CategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿
+
+using AutoMapper;
+using Book.Core.Entities;
+using Book.Service.Dtos.Categories;
+
+namespace Book.Service.Profiles.Categories
+{
+    public class CategoryProfile:Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryPostDto, Category>();
+            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<Category, CategoryGetDto>();
+        }
+    }
+}
