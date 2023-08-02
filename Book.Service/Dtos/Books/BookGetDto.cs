513f2ab8df165a9f1d71e0e65a3befa463cbd5c3
İlkin <ilkinhd@code.edu.az>
2023-08-02 18:29:20 +0400

    Account  Dtos are added

--- Book.Service/Dtos/Books/BookGetDto.cs
+++ Book.Service/Dtos/Books/BookGetDto.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Book.Service.Dtos.Books
 {
-    internal class BookGetDto
+    public record BookGetDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+        public string ImageURL { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
--- Book.Service/Dtos/Books/BookUpdateDto.cs
+++ Book.Service/Dtos/Books/BookUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Book.Service.Dtos.Books
 {
-    internal class BookUpdateDto
+    public record BookUpdateDto
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public IFormFile File { get; set; }
+        public int CategoryId { get; set; }
     }
 }
