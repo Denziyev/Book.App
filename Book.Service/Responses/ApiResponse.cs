28dc4fbde55e01ed4651873455cf973e4798f4c6
İlkin <ilkinhd@code.edu.az>
2023-08-02 18:33:43 +0400

    Added ApiResponse

---
+++ Book.Service/Responses/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Service.Responses
+{
+    public class ApiResponse
+    {
+        public object items { get; set; }
+        public int StatusCode { get; set; }
+        public string Description { get; set; }
+    }
+}
