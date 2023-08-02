3e64b5d365959e109d1bc9176a5741808b3ba68c
İlkin <ilkinhd@code.edu.az>
2023-08-02 16:32:02 +0400

    Book and Category entity are added

---
+++ Book.Core/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Core.Entities
+{
+    public class Book:BaseEntity
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+        public string ImageURL { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
